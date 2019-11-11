e14ca2358173646ed338bbc5bc32cacdf5fc7995
Joseph Adams <josephdadams@gmail.com>
2019-11-11 11:13:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# LiveCaption
+LiveCaption was written by Joseph Adams and is distributed under the MIT License.
+
+Copyright 2019 Joseph Adams.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+It is not sold, authorized, or associated with any other company or product.
+
+To contact the author or for more information, please visit [www.techministry.blog](http://www.techministry.blog).
+
+## ABOUT THIS SOFTWARE
+This software will run a webserver allowing the user to transcript live audio to text and relay that to many clients at once, in real time.
+
+## INSTALLATION
+1. Download the latest source code/release files from the repository.
+1. Make sure Node.js is installed.
+1. In the terminal, from the path you placed the release files, type: `npm install` to install any necessary dependencies/libaries.
+1. Then, in the terminal, type `node index.js`. **The server will run on port 3000 by default.**
+
+## CONFIGURATION
+1. Go to the Config page on the server running *LiveCaption*: `http://127.0.0.1:3000/config` It runs on port 3000 by default.
+1. You will be prompted for the Config page login. The default username is *config* and the default password is *config22*.
+1. This page enables you to Add/Edit Bridges. Bridges are the "rooms" that viewers can join to view captions. You can think of them as locations or venues.
+1. You can also change the login password for both the Config and Bridge Control pages.
+1. You also have the option to set a Global Logo. There is one included in the default dataset, but if you want custom branding, upload your own logo.
+
+### ADDING/EDITING BRIDGES
+1. Each Bridge is auto-assigned a unique ID field.
+1. You can assign a custom name.
+1. The control password, if populated, will require anyone wishing to control this Bridge from the Bridge control page to have to enter the password in order to take control.
+1. The observe password, if populated, will require anyone wishing to view the captions to have to enter the password in order to join the room.
+1. The bridge can be enabled or disabled. This is helpful if you wish to have a lot of Bridge rooms configured but not all of them always available.
+1. A custom logo for the Bridge can be uploaded. This is used when you put the Bridge in Logo Mode or if a client connects when no one is controlling that particular Bridge (it's offline).
+1. You can set a custom foreground text color, background color, and font.
+
+## CONTROLLING A BRIDGE
+1. **In a Google Chrome browser**, go to the Bridge page on the server running *LiveCaption*: `http://127.0.0.1:3000/bridge` It runs on Port 3000 by default. Chrome is required for Bridge Control because it is the only browser that has this client-side API to convert audio to text.
+1. You will be prompted for the Bridge Page login. If you are using the default configuration, the username is **bridge** and the password is **bridge22**. This can be changed in the settings.
+1. Select a Bridge from the Bridge Config box. If it requires a control password, you will need to enter it. The default dataset includes a Bridge named Bridge1 and does not include a password.
+1. Click "Connect to Bridge" to connect. If a Bridge is already in use, you will receive an error message.
+1. To add bridges or make changes to existing bridges like adding a logo, changing observe/control passwords, etc., either go to */config* or click the "Settings" icon under the Bridge config section.
+The default username is *config* and the default password is *config22*.
+1. After connecting and taking control of a Bridge room, the Logo Mode is enabled by default. To turn off the logo, simply toggle the Logo On to Logo Off.
+1. When you are ready to start converting audio to text, click "Start Listening" under *Caption Config*. To stop converting, click "Stop Listening" at any time. **If your connection is not HTTPS and you are not viewing the Bridge Control page as localhost, Chrome will not be allowed to access the microphone due to the secure origin policy.
+> To ignore Chrome’s secure origin policy, follow these steps.
+>	* In the Chrome address bar, navigate to `chrome://flags/#unsafely-treat-insecure-origin-as-secure`.
+>	* Find and enable the `Insecure origins treated as secure` section.
+>	* Add any addresses you want to ignore the secure origin policy for. Remember to include the port number as well.
+>	* Save and restart Chrome.
+1. A box will appear on the right side of the screen that contains the transcripted audio. By default, this data will not be sent to the clients until you toggle "Do Not Send" over to "Send Data". This is helpful if you want to test the transcription process and make any necessary adjustments on the audio side before sending the text to clients. You can also continue listening to audio and transcribe it but not send the data to clients, if desired.
+1. To redirect all connected clients to another page (disconnecting them from the Bridge room in the process), you can type in a URL and click "Redirect to URL". Once they're redirected, they're gone.
+1. You can send a text/announcement at any time and it wil appear at the top location bar on all connected clients.
+1. You can also remotely reload all listeners, which will disconnect them from your Bridge room and ask them to choose a Bridge room again.
+1. There is a simple Word Dictionary function at the bottom of the page where you can enter words or phrases and the replacement words or phrases to use instead. This is helpful if your audio source is often transcribed incorrectly.
+
+## VIEWING CAPTIONS
+1. Go to the index page of the server running *LiveCaption*: `http://127.0.0.1:3000/`
+1. A list of Bridges to select and join will be presented. If there is only one Bridge configured/enabled, that one will be joined automatically and no choices will be given.
+1. Captions will appear in real time as the data is sent from the Bridge Control page.
+
+## USING THE API FOR BRIDGE CONTROL
+There is a simple API available to access certain Bridge Control functions.
+
+Send an HTTP POST request to `/api/bridgecontrol`, with a JSON object (Content Type of `application/json`).
+The object should contain these values:
+1. `bridgeID`: The unique ID of the Bridge. You can get this from the Config page if you don't know the ID.
+2. `command`: The API command to send.
+3. `password`: The control password. Send "" if the password is blank.
+
+### AVAILABLE COMMANDS
+* `startlistening`: Starts listening to incoming audio.
+* `stoplistening`: Stops listening to incoming audio.
+* `senddata`: Sends data to connected clients (send the transcribed text).
+* `stopdata`: Stops sending data.
+* `logoon`: Enables Logo Mode.
+* `logooff`: Disables Logo Mode.
+
+*Example:*
+```javascript
+{
+	bridgeID: "5d0b9ca7f",
+	command: "startlistening",
+	password: "control22"
+}
+```
+
+`curl -d '{"bridgeID":"5d0b9ca7f","command":"startlistening","password":"control22"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:3000/api/bridgecontrol`
+
+If the command is sent successfully, you will receive a response of `command-sent`.
+Other responses can include:
+* `invalid-bridgeid`: The bridgeID you sent was not found (or was not included in the request).
+* `bridge-not-inuse`: The bridge you attempted to control was not currently in use/in control (No one was connected to that Bridge from the Bridge Control page).
+* `invalid-command`: The command you sent was not one of the options.
+* `invalid-password`: The control password you sent was incorrect.
---
+++ index.js
@@ -0,0 +1,632 @@
+/* LiveCaption */
+
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const defaultListenPort = 3000;
+
+const JSONdatafile = 'livecaption-data.json';
+
+const ConfigLogin = 'config';
+var ConfigPassword = 'config22';
+const ConfigPassword_Default = 'config22';
+
+var GlobalLogo = '';
+
+const BridgeLogin = 'bridge';
+var BridgePassword = 'bridge22';
+const BridgePassword_Default = 'bridge22';
+
+var Bridges = []; //array of bridges/locations available
+
+var Clients = []; //array of people currently connected across entire service
+
+var WordDictionary = []; //word replacement dictionary array
+
+var configSecureRoute = require('express').Router();
+
+app.use(bodyParser.json({ type: 'application/json' }));
+
+configSecureRoute.use((req, res, next) => {
+
+	// -----------------------------------------------------------------------
+	// authentication middleware
+
+	// parse login and password from headers
+	const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+	const [login, password] = new Buffer(b64auth, 'base64').toString().split(':');
+
+	// Verify login and password are set and correct
+	if (!login || !password || login !== ConfigLogin || password !== ConfigPassword) {
+		res.set('WWW-Authenticate', 'Basic realm=\'401\''); // change this
+		res.status(401).send('Authentication required to access this area.'); // custom message
+		return;
+	}
+
+	// -----------------------------------------------------------------------
+	// Access granted...
+	next();
+});
+
+var bridgeSecureRoute = require('express').Router();
+
+bridgeSecureRoute.use((req, res, next) => {
+
+	// -----------------------------------------------------------------------
+	// authentication middleware
+
+	// parse login and password from headers
+	const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+	const [login, password] = new Buffer(b64auth, 'base64').toString().split(':');
+
+	// Verify login and password are set and correct
+	if (!login || !password || login !== BridgeLogin || password !== BridgePassword) {
+		res.set('WWW-Authenticate', 'Basic realm=\'401\''); // change this
+		res.status(401).send('Authentication required to access this area.'); // custom message
+		return;
+	}
+
+	// -----------------------------------------------------------------------
+	// Access granted...
+	next();
+});
+
+//config page
+configSecureRoute.get('/', function (req, res) {
+	res.sendFile(__dirname + '/views/config.html');
+});
+
+app.use('/config', configSecureRoute);
+
+//bridge page
+bridgeSecureRoute.get('/', function (req, res) {
+	res.sendFile(__dirname + '/views/bridge.html');
+});
+
+app.use('/bridge', bridgeSecureRoute);
+
+//about page
+app.get('/about', function (req, res) {
+	res.sendFile(__dirname + '/views/about.html');
+});
+
+//listener page
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+//API functions
+app.post('/api/bridgecontrol', function (req, res) {
+	let obj = req.body;
+	
+	let response = '';
+	
+	if (obj.bridgeID) {
+		switch(obj.command) {
+			case 'startlistening':
+			case 'stoplistening':
+			case 'senddata':
+			case 'stopdata':
+			case 'logoon':
+			case 'logooff':
+				response = ControlBridge(obj.bridgeID, obj.command, obj.password);
+				break;
+			default:
+				response = 'invalid-command';
+		}
+	}
+	else {
+		response = 'invalid-bridgeid';
+	}
+	
+	res.send({returnStatus: response});
+});
+
+//static images and libraries
+app.use(express.static('views/static'));
+
+//socket.io sockets
+io.sockets.on('connection', function(socket) {
+	// Config Sockets
+	
+	//Get All Bridges
+	socket.on('config_bridgeroom_getall', function() {
+		socket.emit('config_bridgerooms', Bridges);
+	});
+	
+	//Return Current Global Logo
+	socket.on('config_getgloballogo', function () {
+		socket.emit('config_globallogo', GlobalLogo);
+	});
+	
+	//Add New Bridge
+	socket.on('config_bridgeroom_add', function (bridgeRoomObj) {
+		if (bridgeRoomObj.logo === '') {
+			bridgeRoomObj.logo = '';
+		}
+		Bridges.push(bridgeRoomObj);
+		io.to('Config').emit('config_status', 'add-success');
+		io.to('Config').emit('config_bridgerooms', Bridges);
+		io.to('BridgeRooms').emit('bridgerooms', Bridges);
+		saveFile();
+	});
+	
+	//Update Bridge
+	socket.on('config_bridgeroom_update', function (bridgeRoomObj) {
+		let index = null;
+		for (let i = 0; i < Bridges.length; i++) {
+			if (Bridges[i].id === bridgeRoomObj.id) {
+				index = i;
+				break;
+			}
+		}
+		if (index !== null) {
+			Bridges.splice(index, 1);
+			Bridges.push(bridgeRoomObj);
+			io.to('Config').emit('config_status', 'update-success');
+			io.to('BridgeRooms').emit('bridgerooms', Bridges);
+			saveFile();
+		}
+		else {
+			io.to('Config').emit('config_status', 'update-failure');
+		}
+	});
+	
+	//Delete Bridge
+	socket.on('config_bridgeroom_delete', function (bridgeRoomObj) {
+		let index = null;
+		for (let i = 0; i < Bridges.length; i++) {
+			if (Bridges[i].id === bridgeRoomObj.id) {
+				index = i;
+				break;
+			}
+		}
+		if (index !== null) {
+			Bridges.splice(index, 1);
+			io.to('Config').emit('config_status', 'delete-success');
+			io.to('BridgeRooms').emit('bridgerooms', Bridges);
+			saveFile();
+		}
+		else {
+			io.to('Config').emit('config_status', 'delete-failure');
+		}
+	});
+	
+	//Change Config Page Password
+	socket.on('config_changeconfigpassword', function(oldPassword, newPassword) {
+		if (oldPassword === ConfigPassword) {
+			ConfigPassword = newPassword;
+			saveFile();
+			socket.emit('config_status', 'changeconfigpassword-success');
+		}
+		else {
+			socket.emit('config_status', 'changeconfigpassword-failure');
+		}
+	});
+	
+	//Change Bridge Page Password
+	socket.on('config_changebridgepassword', function(oldPassword, newPassword) {
+		if (oldPassword === BridgePassword) {
+			BridgePassword = newPassword;
+			saveFile();
+			socket.emit('config_status', 'changebridgepassword-success');
+		}
+		else {
+			socket.emit('config_status', 'changebridgepassword-failure');
+		}
+	});
+	
+	//Change Global Logo
+	socket.on('config_globallogo_update', function(logo) {
+		GlobalLogo = logo;
+		saveFile();
+		socket.emit('config_status', 'globallogo_update');
+	});
+	
+	// Bridge Sockets
+	
+	// Return all bridges in the array
+	socket.on('bridgerooms_getall', function () {
+		let bridgeArray = GetCleanBridgeArray();
+		socket.emit('bridgerooms', bridgeArray); 
+	});
+	
+	//Authenticate a Bridge and set it in use
+	socket.on('bridgerooms_authenticate', function(bridgeID, password) {
+		if (GetBridgeInUse(bridgeID)) {
+			socket.emit('bridgerooms_inuse', true);
+		}
+		else {
+			let controlPassword = null;
+			for (let i = 0; i < Bridges.length; i++) {
+				if (Bridges[i].id === bridgeID) {
+					controlPassword = Bridges[i].controlPassword;
+					break;
+				}
+			}
+
+			if (password === controlPassword) {
+				socket.join('BridgeRooms'); // the room with all of the authenticated bridges in it
+				socket.join('BridgeRoom-' + bridgeID); //this specific room that listener clients will receive data from
+				socket.emit('bridgerooms_authenticated', true);
+				socket.emit('bridgerooms_selectedbridge', bridgeID);
+				socket.emit('word_dictionary', getWordsByBridgeID(bridgeID));
+				AddClient(socket, bridgeID, 'Bridge');
+				SetBridgeInUse(bridgeID, true);
+				SetLogoMode(bridgeID, true);
+			}
+			else {
+				socket.emit('bridgerooms_authenticated', false);
+			}
+		}
+	});
+	
+	//Disconnect from Bridge
+	socket.on('bridgerooms_disconnect', function(bridgeID) {
+		SetBridgeInUse(bridgeID, false);
+		SetLogoMode(bridgeID, true);
+	});
+	
+	socket.on('announcement', function(bridgeID, text) {
+		updateListener(bridgeID, 'announcement', text);
+	});
+	
+	socket.on('redirect', function(bridgeID, url) {
+		updateListener(bridgeID, 'redirect', url);
+	});
+	
+	socket.on('gotologo', function (bridgeID, value) {
+		SetLogoMode(bridgeID, value);
+		updateListener(bridgeID, 'gotologo', value);
+	});
+	
+	socket.on('keepawake', function (bridgeID, value) {
+		updateListener(bridgeID, 'keepawake', value);
+	});
+	
+	socket.on('reload', function(bridgeID, value) {
+		updateListener(bridgeID, 'reload', value);
+	});
+	
+	socket.on('clearcaptions', function(bridgeID) {
+		updateListener(bridgeID, 'clearcaptions', true);
+	});
+	
+	socket.on('newcaption', function(bridgeID, finalTranscript, interimTranscript) {
+		updateCaptions(bridgeID, finalTranscript, interimTranscript);
+	});
+	
+	socket.on('addword', function(bridgeID, strWordToReplace, strNewWord) {
+		addWord(bridgeID, strWordToReplace, strNewWord);
+	});
+	
+	socket.on('deleteword', function(bridgeID, strWord) {
+		deleteWord(bridgeID, strWord);
+		socket.emit('word_dictionary', getWordsByBridgeID(bridgeID));
+	});
+	
+	// LISTENER SOCKETS
+	
+	//Join the selected Bridge Room
+	socket.on('listener_joinbridgeroom', function(bridgeID, password) {
+		for (let i = 0; i < Bridges.length; i++) {
+			if (Bridges[i].id === bridgeID) {
+				if (Bridges[i].observePassword === password) {
+					socket.join('Listener-' + bridgeID);
+					socket.emit('status', 'success');
+					socket.emit('gotologo', GetLogoMode(bridgeID));
+					socket.emit('bridgeinuse', GetBridgeInUse(bridgeID));
+					AddClient(socket, bridgeID, 'Listener');
+				}
+				else {
+					socket.emit('status', 'failure');
+				}
+				break;
+			}
+		}
+	});
+	
+	//Other Sockets
+	socket.on('room', function(room) {
+		switch(room) {
+			case 'Listeners':
+				socket.join(room);
+				socket.emit('bridgerooms', GetAvailableBridges());
+				socket.emit('globallogo', GlobalLogo);
+				break;
+			case 'BridgeRooms':
+				socket.join(room);
+				break;
+			case 'Config':
+				socket.join(room);
+				if (ConfigPassword === ConfigPassword_Default)
+				{
+					socket.emit('config_passwordisdefault', true);
+				}
+				break;
+			default:
+				break;
+		}
+	});
+	
+	socket.on('disconnect', function() {
+		RemoveClient(socket);
+		
+		io.to('Bridge').emit('client_disconnected', Clients);
+	});
+});
+
+//Sets the Bridge in use so no one else can use it
+function SetBridgeInUse(bridgeID, value) {
+	for (let i = 0; i < Bridges.length; i++) {
+		if (Bridges[i].id === bridgeID) {
+			Bridges[i].inUse = value;
+			io.to('Listener-' + bridgeID).emit('bridgeinuse', value);
+			break;
+		}
+	}
+}
+
+//Returns true/false if the bridge is currently in use
+function GetBridgeInUse(bridgeID) {
+	let bridgeObj = Bridges.find(function (obj) { return obj.id.toString() === bridgeID; });
+	
+	return bridgeObj.inUse;
+}
+
+//Adds the Listener to the global list of clients
+function AddClient(socket, bridgeID, type) {
+	let clientObj = {};
+	
+	clientObj.socketID = socket.id; //first position is always the socket ID
+	clientObj.bridgeID = bridgeID;
+	clientObj.issued = socket.handshake.issued;
+	clientObj.address = socket.handshake.address;
+	
+	clientObj.roomType = type;
+	
+	Clients.push(clientObj);
+	io.to('BridgeRoom-' + bridgeID).emit('client_connected', clientObj);
+}
+
+//Removes the listener from the global list of clients
+function RemoveClient(socket) {	
+	let socketID = socket.id;
+	let index = null;
+	
+	let bridgeID = null;
+	
+	for (let i = 0; i < Clients.length; i++) {
+		if (Clients[i].socketID === socketID) {
+			index = i;
+			bridgeID = Clients[i].bridgeID;
+
+			if (Clients[i].roomType === 'Bridge') {
+				SetBridgeInUse(bridgeID, false);
+			}
+			
+			break;
+		}
+	}
+	
+	if (index !== null) {
+		Clients.splice(index, 1);
+		io.to('BridgeRoom-' + bridgeID).emit('client_disconnected', socketID);
+	}
+}
+
+//Sets the bridge in logo mode
+function SetLogoMode(bridgeID, value) {
+	for (let i = 0; i < Bridges.length; i++) {
+		if (Bridges[i].id === bridgeID) {
+			Bridges[i].logoMode = value;
+			break;
+		}
+	}
+}
+
+//Returns whether the bridge is in logo mode or not
+function GetLogoMode(bridgeID) {
+	let logoMode = false;
+	
+	for (let i = 0; i < Bridges.length; i++) {
+		if (Bridges[i].id === bridgeID) {
+			logoMode = Bridges[i].logoMode;
+			break;
+		}
+	}
+
+	return logoMode;
+}
+
+//Updates all listeners with the latest command (announcement, redirect, etc.)
+function updateListener(bridgeID, mode, text) {
+	io.to('Listener-' + bridgeID).emit(mode, text);
+}
+
+//Updates all listeners with the latest caption text
+function updateCaptions(bridgeID, finalTranscript, interimTranscript) {
+	io.to('Listener-' + bridgeID).emit('caption', finalTranscript, interimTranscript);
+}
+
+//Adds the word to the Word Dictionary Array
+function addWord(bridgeID, strWordToReplace, strNewWord) {
+	let dictionaryObj = {};
+	dictionaryObj.bridgeID = bridgeID;
+	dictionaryObj.word = strWordToReplace;
+	dictionaryObj.replaceWord = strNewWord;
+	WordDictionary.push(dictionaryObj);
+	saveFile();
+}
+
+function deleteWord(bridgeID, word) {
+	let index = null;
+	for (let i = 0; i < WordDictionary.length; i++) {
+		if ((WordDictionary[i].word === word) && (WordDictionary[i].bridgeID === bridgeID)) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index !== null) {
+		WordDictionary.splice(index, 1);
+		saveFile();
+	}
+}
+
+function getWordsByBridgeID(bridgeID)
+{
+	let wordArray = [];
+	
+	for (let i = 0; i < WordDictionary.length; i++) {
+		if (WordDictionary[i].bridgeID === bridgeID) {
+			let wordObj = {};
+			wordObj.word = WordDictionary[i].word;
+			wordObj.replaceWord = WordDictionary[i].replaceWord;
+			wordArray.push(wordObj);
+		}
+	}
+	
+	return wordArray;
+}
+
+function startUp() {
+	http.listen(defaultListenPort, function () {
+		console.log('LiveCaption Server listening on port:' + defaultListenPort);
+	});
+}
+
+//Loads settings from memory on first startup
+function loadFile() {
+	try {
+		let rawdata = fs.readFileSync(JSONdatafile); 
+		let myJson = JSON.parse(rawdata); 
+
+		Bridges = myJson.Bridges;
+		ConfigPassword = myJson.ConfigPassword;
+		BridgePassword = myJson.BridgePassword;
+		GlobalLogo = myJson.GlobalLogo;
+		WordDictionary = myJson.WordDictionary;
+	}
+	catch (error) {
+		Bridges = [];
+		ConfigPassword = ConfigPassword_Default;
+		BridgePassword = BridgePassword_Default;
+		GlobalLogo = null;
+		WordDictionary = [];
+	}
+}
+
+//Saves settings to a local storage file for later recalling
+function saveFile() {
+	var myJson = {
+		ConfigPassword: ConfigPassword,
+		BridgePassword: BridgePassword,
+		GlobalLogo: GlobalLogo,
+		Bridges: Bridges,
+		WordDictionary: WordDictionary
+	};
+
+	fs.writeFileSync(JSONdatafile, JSON.stringify(myJson), 'utf8', function(error) {
+		if (error) { 
+			console.log('error: ' + error);
+		}
+		else {
+			console.log('file saved');
+		}
+	});
+}
+
+//Returns the bridge data based on the provided bridgeID
+function GetBridge(bridgeID) {
+	let bridgeObj = null;
+	
+	for (let i = 0; i < Bridges.length; i++) {
+		if (Bridges[i].id === bridgeID) {
+			bridgeObj = Bridges[i];
+		}
+	}
+	
+	return bridgeObj;
+}
+
+//Builds an array of current Bridges but only with the ID and Name, so no passwords and other data are sent unncessarily
+function GetCleanBridgeArray() {
+	let bridgeArray = [];
+	
+	for (let i = 0; i < Bridges.length; i++) {
+		if (Bridges[i].enabled) {
+			let bridgeObj = {};
+			bridgeObj.id = Bridges[i].id;
+			bridgeObj.name = Bridges[i].name;
+			bridgeObj.logo = Bridges[i].logo;
+			bridgeObj.inUse = Bridges[i].inUse;
+			if (Bridges[i].controlPassword !== '') {
+				bridgeObj.requiresPassword = true;
+			}
+			else {
+				bridgeObj.requiresPassword = false;
+			}
+			bridgeArray.push(bridgeObj);
+		}
+	}
+	
+	return bridgeArray;
+}
+
+//Builds an array of bridges that are enabled
+function GetAvailableBridges() {
+	let bridgeArray = [];
+	
+	for (let i = 0; i < Bridges.length; i++) {
+		if (Bridges[i].enabled) {
+			let bridgeObj = {};
+			bridgeObj.id = Bridges[i].id;
+			bridgeObj.name = Bridges[i].name;
+			bridgeObj.logo = Bridges[i].logo;
+			bridgeObj.logoMode = Bridges[i].logoMode;
+			bridgeObj.inUse = Bridges[i].inUse;
+			bridgeObj.foregroundColor = Bridges[i].foregroundColor;
+			bridgeObj.backgroundColor = Bridges[i].backgroundColor;
+			bridgeObj.font = Bridges[i].font;
+			if (Bridges[i].observePassword !== '') {
+				bridgeObj.requiresPassword = true;
+			}
+			else {
+				bridgeObj.requiresPassword = false;
+			}
+			bridgeArray.push(bridgeObj);
+		}
+	}
+	
+	return bridgeArray;
+}
+
+//API control - sends the specified command to the bridge room
+function ControlBridge(bridgeID, command, password) {
+	let bridgeObj = GetBridge(bridgeID);
+	
+	if (bridgeObj) {
+		if (bridgeObj.controlPassword === password) {
+			if (GetBridgeInUse(bridgeID)) {
+				io.to('BridgeRoom-' + bridgeID).emit('bridge_control', command);
+				return 'command-sent';
+			}
+			else {
+				return 'bridge-not-inuse';
+			}
+		}
+		else {
+			return 'invalid-password';
+		}
+	}
+	else {
+		return 'invalid-bridgeid';
+	}
+}
+
+loadFile();
+startUp();
